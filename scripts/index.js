11813099a033c19b86882933f93f21d606001d32
KKian004 <102926185+KKian004@users.noreply.github.com>
2024-02-04 22:46:01 +0300

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -4,8 +4,8 @@ const giftbox = document.getElementById('merrywrap');
 const canvasC = document.getElementById('c');
 
 const config = {
-  birthdate: 'NOV 17, 2023',
-  name: 'LYDIA'
+  birthdate: 'FEB 5, 2024',
+  name: 'JUDE'
 };
 
 function hideEverything() {
