ffddac8161d16710209414e04c3c2ffa3cf93928
KKian004 <102926185+KKian004@users.noreply.github.com>
2024-02-04 23:10:45 +0300

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -4,7 +4,7 @@ const giftbox = document.getElementById('merrywrap');
 const canvasC = document.getElementById('c');
 
 const config = {
-  birthdate: 'FEB 5, 2024',
+  birthdate: 'FEB 5, 2023',
   name: 'JUDE'
 };
 
